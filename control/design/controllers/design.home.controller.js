ebe699d98d012a437cbd095b6ea6539158ae5d95
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-05-05 15:48:20 +0530

    Angularized design section

---
+++ control/design/controllers/design.home.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function (angular, window) {
+  angular
+    .module('seminarNotesPluginDesign')
+    .controller('DesignHomeCtrl', ['$scope', 'Buildfire', 'LAYOUTS', 'DataStore', 'TAG_NAMES',
+      function ($scope, Buildfire, LAYOUTS, DataStore, TAG_NAMES) {
+
+      }]);
+})(window.angular);
---
+++ control/design/enums.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function (angular) {
+  angular.module('seminarNotesPluginDesign')
+    .constant('TAG_NAMES', {
+      SEMINAR_INFO: 'seminarInfo',
+      SEMINAR_ITEMS: "seminarItems"
+    })
+    .constant('STATUS_CODE', {
+      INSERTED: 'inserted',
+      UPDATED: 'updated',
+      NOT_FOUND: 'NOTFOUND',
+      UNDEFINED_DATA: 'UNDEFINED_DATA',
+      UNDEFINED_OPTIONS: 'UNDEFINED_OPTIONS',
+      UNDEFINED_ID: 'UNDEFINED_ID',
+      ITEM_ARRAY_FOUND: 'ITEM_ARRAY_FOUND',
+      NOT_ITEM_ARRAY: 'NOT_ITEM_ARRAY'
+    })
+    .constant('STATUS_MESSAGES', {
+      UNDEFINED_DATA: 'Undefined data provided',
+      UNDEFINED_OPTIONS: 'Undefined options provided',
+      UNDEFINED_ID: 'Undefined id provided',
+      NOT_ITEM_ARRAY: 'Array of Items not provided',
+      ITEM_ARRAY_FOUND: 'Array of Items provided'
+    })
+    .constant('LAYOUTS', {
+      itemListLayout: [
+        {name: "Item_List_1"},
+        {name: "Item_List_2"},
+        {name: "Item_List_3"},
+        {name: "Item_List_4"}
+      ]
+    });
+})(window.angular);
