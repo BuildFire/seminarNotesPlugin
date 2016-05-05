8c847e274ed3909cc381d097d3627f3a8fe9068a
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-05-05 16:17:55 +0530

    Angularized widget section

---
+++ widget/controllers/widget.home.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function (angular, buildfire) {
+  angular.module('seminarNotesPluginWidget')
+    .controller('WidgetHomeCtrl', ['$scope', 'TAG_NAMES', 'LAYOUTS', 'DataStore', 'PAGINATION', 'Buildfire', 'Location', '$rootScope',
+      function ($scope, TAG_NAMES, LAYOUTS, DataStore, PAGINATION, Buildfire, Location, $rootScope) {
+
+      }])
+})(window.angular, window.buildfire);
