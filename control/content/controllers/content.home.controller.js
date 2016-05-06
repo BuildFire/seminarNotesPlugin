a608f0da9c5d17b41482f951e2b51b6352975339
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-05-06 14:51:11 +0530

    Added carousel and wysiwyg for content home page

--- control/content/controllers/content.home.controller.js
+++ control/content/controllers/content.home.controller.js
@@ -3,8 +3,68 @@
 (function (angular, buildfire) {
   angular
     .module('seminarNotesPluginContent')
-    .controller('ContentHomeCtrl', ['$scope', 'TAG_NAMES', 'STATUS_CODE', 'DataStore', 'LAYOUTS', '$sce', 'PAGINATION', 'Buildfire', '$modal','$rootScope',
+    .controller('ContentHomeCtrl', ['$scope', 'TAG_NAMES', 'STATUS_CODE', 'DataStore', 'LAYOUTS', '$sce', 'PAGINATION', 'Buildfire', '$modal', '$rootScope',
       function ($scope, TAG_NAMES, STATUS_CODE, DataStore, LAYOUTS, $sce, PAGINATION, Buildfire, $modal, $rootScope) {
 
+        var ContentHome = this;
+
+        var _data = {
+          "content": {
+            "carouselImages": [],
+            "description": ""
+          },
+          "design": {
+            "itemListLayout": LAYOUTS.itemListLayout[0].name,
+            "itemListBgImage": ""
+          }
+        };
+
+        ContentHome.descriptionWYSIWYGOptions = {
+          plugins: 'advlist autolink link image lists charmap print preview',
+          skin: 'lightgray',
+          trusted: true,
+          theme: 'modern'
+        };
+
+        // create a new instance of the buildfire carousel editor
+        var editor = new Buildfire.components.carousel.editor("#carousel");
+
+        // this method will be called when a new item added to the list
+        editor.onAddItems = function (items) {
+          if (!ContentHome.data.content.carouselImages)
+            ContentHome.data.content.carouselImages = [];
+          ContentHome.data.content.carouselImages.push.apply(ContentHome.data.content.carouselImages, items);
+          $scope.$digest();
+        };
+        // this method will be called when an item deleted from the list
+        editor.onDeleteItem = function (item, index) {
+          ContentHome.data.content.carouselImages.splice(index, 1);
+          $scope.$digest();
+        };
+        // this method will be called when you edit item details
+        editor.onItemChange = function (item, index) {
+          ContentHome.data.content.carouselImages.splice(index, 1, item);
+          $scope.$digest();
+        };
+        // this method will be called when you change the order of items
+        editor.onOrderChange = function (item, oldIndex, newIndex) {
+          var items = ContentHome.data.content.carouselImages;
+
+          var tmp = items[oldIndex];
+
+          if (oldIndex < newIndex) {
+            for (var i = oldIndex + 1; i <= newIndex; i++) {
+              items[i - 1] = items[i];
+            }
+          } else {
+            for (var i = oldIndex - 1; i >= newIndex; i--) {
+              items[i + 1] = items[i];
+            }
+          }
+          items[newIndex] = tmp;
+
+          ContentHome.data.content.carouselImages = items;
+          $scope.$digest();
+        };
       }]);
 })(window.angular, window.buildfire);
