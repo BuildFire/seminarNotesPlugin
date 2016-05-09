39d53d38689914e5f1f7845167b2ca8127c37a73
tabish.khan <tabish.khan@tothenew.com>
2016-05-09 11:53:10 +0530

    Added view stack and set the initial layout layout for widget section

--- widget/app.js
+++ widget/app.js
@@ -1,163 +1,127 @@
 'use strict';
 
 (function (angular, buildfire, window) {
-  angular.module('seminarNotesPluginWidget', ['ngRoute', 'ui.bootstrap'])
-    .config(['$routeProvider', '$compileProvider', function ($routeProvider, $compileProvider) {
+    angular.module('seminarNotesPluginWidget', ['ngRoute', 'ui.bootstrap'])
+        .config(['$routeProvider', '$compileProvider', function ($routeProvider, $compileProvider) {
 
-      /**
-       * To make href urls safe on mobile
-       */
-      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|cdvfile|file):/);
+            /**
+             * To make href urls safe on mobile
+             */
+            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|cdvfile|file):/);
 
 
-      $routeProvider
-        .when('/', {
-          template: '<div></div>'
-        })
-        .when('/event/:id', {
-          templateUrl: 'templates/eventDetails.html',
-          controller: 'WidgetEventCtrl',
-          controllerAs: 'WidgetEvent'
-        })
-        .otherwise('/');
-    }])
-    .filter('getMonth', function () {
-      var monthsObj = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-      return function (input) {
-        return monthsObj[new Date(input).getMonth()];
-      };
-    })
-    .filter('getDate', function () {
-      return function (input) {
-        return new Date(input).getDate();
-      };
-    })
-    .filter('getImageUrl', function () {
-      return function (url, width, height, type) {
-        if (type == 'resize')
-          return buildfire.imageLib.resizeImage(url, {
-            width: width,
-            height: height
-          });
-        else
-          return buildfire.imageLib.cropImage(url, {
-            width: width,
-            height: height
-          });
-      }
-    })
-    .filter('getTimeZone', function () {
-      var timezone = jstz.determine();
-      console.log(timezone.name());
-      return function (input) {
-        return moment.tz(timezone.name()).format("z");
-      };
-    })
-    .directive("buildFireCarousel", ["$rootScope", "$timeout", function ($rootScope, $timeout) {
-      return {
-        restrict: 'A',
-        link: function (scope, elem, attrs) {
-          $timeout(function () {
-            $rootScope.$broadcast("Carousel:LOADED");
-          });
-        }
-      };
-    }])
-    .directive("googleMap", function () {
-      return {
-        template: "<div></div>",
-        replace: true,
-        scope: {coordinates: '='},
-        link: function (scope, elem, attrs) {
-          scope.$watch('coordinates', function (newValue, oldValue) {
-            if (newValue) {
-              scope.coordinates = newValue;
-              if (scope.coordinates.length) {
-                var map = new google.maps.Map(elem[0], {
-                  center: new google.maps.LatLng(scope.coordinates[1], scope.coordinates[0]),
-                  zoomControl: false,
-                  streetViewControl: false,
-                  mapTypeControl: false,
-                  zoom: 15,
-                  mapTypeId: google.maps.MapTypeId.ROADMAP
-                });
-                var marker = new google.maps.Marker({
-                  position: new google.maps.LatLng(scope.coordinates[1], scope.coordinates[0]),
-                  map: map
-                });
-                var styleOptions = {
-                  name: "Report Error Hide Style"
-                };
-                var MAP_STYLE = [
-                  {
-                    stylers: [
-                      {visibility: "on"}
-                    ]
-                  }];
-                var mapType = new google.maps.StyledMapType(MAP_STYLE, styleOptions);
-                map.mapTypes.set("Report Error Hide Style", mapType);
-                map.setMapTypeId("Report Error Hide Style");
+        }])
+        .directive("viewSwitcher", ["ViewStack", "$rootScope", '$compile', "$templateCache",
+            function (ViewStack, $rootScope, $compile, $templateCache) {
+                return {
+                    restrict: 'AE',
+                    link: function (scope, elem, attrs) {
+                        var views = 0;
+                        manageDisplay();
+                        $rootScope.$on('VIEW_CHANGED', function (e, type, view) {
+                            if (type === 'PUSH') {
+                                var newScope = $rootScope.$new();
+                                newScope.currentItemListLayout = "templates/" + view.template + ".html";
 
-                marker.addListener('click', function () {
+                                var _newView = '<div  id="' + view.template + '" ><div class="slide content" data-back-img="{{backgroundImage}}" ng-if="currentItemListLayout" ng-include="currentItemListLayout"></div></div>';
+                                if (view.params && view.params.controller) {
+                                    _newView = '<div id="' + view.template + '" ><div class="slide content" data-back-img="{{backgroundImage}}" ng-if="currentItemListLayout" ng-include="currentItemListLayout" ng-controller="' + view.params.controller + '" ></div></div>';
+                                }
+                                var parTpl = $compile(_newView)(newScope);
+                                if (view.params && view.params.shouldUpdateTemplate) {
+                                    newScope.$on("ITEM_LIST_LAYOUT_CHANGED", function (evt, layout, needDigest) {
+                                        newScope.currentItemListLayout = "templates/" + layout + ".html";
+                                        if (needDigest) {
+                                            newScope.$digest();
+                                        }
+                                    });
+                                }
+                                $(elem).append(parTpl);
+                                views++;
 
-                  buildfire.getContext(function (err, context) {
-                    if (context) {
-                      if (context.device && context.device.platform == 'ios')
-                        window.open("maps://maps.google.com/maps?daddr=" + scope.coordinates[1] + "," + scope.coordinates[0]);
-                      else
-                        window.open("http://maps.google.com/maps?daddr=" + scope.coordinates[1] + "," + scope.coordinates[0]);
+                            } else if (type === 'POP') {
+                                var _elToRemove = $(elem).find('#' + view.template),
+                                    _child = _elToRemove.children("div").eq(0);
+
+                                _child.addClass("ng-leave ng-leave-active");
+                                _child.one("webkitTransitionEnd transitionend oTransitionEnd", function (e) {
+                                    _elToRemove.remove();
+                                    views--;
+                                });
+
+                                //$(elem).find('#' + view.template).remove();
+                            }
+                            else if (type === 'POPALL') {
+                                console.log(view);
+                                angular.forEach(view, function (value, key) {
+                                    $(elem).find('#' + value.template).remove();
+                                });
+                                views = 0;
+                            }
+                            manageDisplay();
+                        });
+
+                        function manageDisplay() {
+                            if (views) {
+                                $(elem).removeClass("ng-hide");
+                            } else {
+                                $(elem).addClass("ng-hide");
+                            }
+                        }
+
                     }
-                  });
-                });
-              }
+                };
+            }])
+        .filter('getImageUrl', function () {
+            return function (url, width, height, type) {
+                if (type == 'resize')
+                    return buildfire.imageLib.resizeImage(url, {
+                        width: width,
+                        height: height
+                    });
+                else
+                    return buildfire.imageLib.cropImage(url, {
+                        width: width,
+                        height: height
+                    });
             }
-          }, true);
-        }
-      }
-    })
-    .directive("loadImage", [function () {
-      return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-          element.attr("src", "../../../styles/media/holder-" + attrs.loadImage + ".gif");
+        })
 
-          var elem = $("<img>");
-          elem[0].onload = function () {
-            element.attr("src", attrs.finalSrc);
-            elem.remove();
-          };
-          elem.attr("src", attrs.finalSrc);
-        }
-      };
-    }])
-    .run(['Location', '$location', '$rootScope', function (Location, $location, $rootScope) {
+        .directive("buildFireCarousel", ["$rootScope", "$timeout", function ($rootScope, $timeout) {
+            return {
+                restrict: 'A',
+                link: function (scope, elem, attrs) {
+                    $timeout(function () {
+                        $rootScope.$broadcast("Carousel:LOADED");
+                    });
+                }
+            };
+        }])
 
-      buildfire.messaging.onReceivedMessage = function (msg) {
-        console.log('$location--------------------------------------------', $location, msg);
-        switch (msg.type) {
-          case 'AddNewItem':
-            Location.goTo("#/event/" + msg.id + "?stopSwitch=true");
-            break;
-          case 'OpenItem':
-            Location.goTo("#/event/" + msg.id);
-            break;
-          default:
-            if ($rootScope.showFeed == false)
-              Location.goToHome();
-        }
-      };
+        .directive("loadImage", [function () {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    element.attr("src", "../../../styles/media/holder-" + attrs.loadImage + ".gif");
 
-      buildfire.navigation.onBackButtonClick = function () {
-        var reg = /^\/event/;
-        if (reg.test($location.path()) && $rootScope.showFeed == false) {
-          buildfire.messaging.sendMessageToControl({});
-          //   buildfire.history.pop();
-          $rootScope.showFeed = true;
-          Location.goTo('#/');
-        }
-        else {
-          buildfire.navigation._goBackOne();
-        }
-      };
-      }]);
+                    var elem = $("<img>");
+                    elem[0].onload = function () {
+                        element.attr("src", attrs.finalSrc);
+                        elem.remove();
+                    };
+                    elem.attr("src", attrs.finalSrc);
+                }
+            };
+        }])
+        .run(['ViewStack', function (ViewStack) {
+            buildfire.navigation.onBackButtonClick = function () {
+                if (ViewStack.hasViews()) {
+                    ViewStack.pop();
+                } else {
+                    buildfire.navigation._goBackOne();
+                }
+            };
+        }]);
+
+
 })(window.angular, window.buildfire, window);
--- widget/controllers/widget.home.controller.js
+++ widget/controllers/widget.home.controller.js
@@ -1,9 +1,14 @@
 'use strict';
 
 (function (angular, buildfire) {
-  angular.module('seminarNotesPluginWidget')
-    .controller('WidgetHomeCtrl', ['$scope', 'TAG_NAMES', 'LAYOUTS', 'DataStore', 'PAGINATION', 'Buildfire', 'Location', '$rootScope',
-      function ($scope, TAG_NAMES, LAYOUTS, DataStore, PAGINATION, Buildfire, Location, $rootScope) {
-
-      }])
+    angular.module('seminarNotesPluginWidget')
+        .controller('WidgetHomeCtrl', ['$scope', 'TAG_NAMES', 'LAYOUTS', 'DataStore', 'PAGINATION', 'Buildfire', 'Location', '$rootScope', 'ViewStack',
+            function ($scope, TAG_NAMES, LAYOUTS, DataStore, PAGINATION, Buildfire, Location, $rootScope, ViewStack) {
+                var WidgetHome = this;
+                WidgetHome.data = {
+                    design: {
+                        itemList: LAYOUTS.itemListLayout[0].name
+                    }
+                }
+            }])
 })(window.angular, window.buildfire);
