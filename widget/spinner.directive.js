5c135c46d14ea9df433b7fa72a007758c512cd20
attila buturla <abuturla@buildfire.com>
2016-10-20 09:22:35 -0700

    different styling

--- widget/spinner.directive.js
+++ widget/spinner.directive.js
@@ -5,7 +5,7 @@ angular
 function spinner() {
     var directive = {
         link: link,
-        template: '<div class="lazy-spinner lazy-spinner-center"></div>',
+        template: '<div class="lazy-spinner-background"><div class="lazy-spinner lazy-spinner-center"></div></div>',
         restrict: 'E'
     };
     return directive;
